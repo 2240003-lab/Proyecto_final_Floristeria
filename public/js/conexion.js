736652efe7ee2f7095f1c949cb1164e051a17f9c
Anthony <antholgtm@gmail.com>
2026-05-01 01:18:35 -0400

    Sexto commit: En este commit se ha iniciado el proyecto final, se iniciola conexion con la base de datos y se empezo a trabajar con el view de la pagina web, osea, la parte visual, al menos con el html. Se realizo un html del inicio de sesion y sobre el registrar. Se organizaron todas las carpetas segun a como realmente se deberia de ver, todos agrupados por grupos. Todo se esta realizando primero en html para que luego se pueda pasar a hbs.

---
+++ public/js/conexion.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '14@nthoD0510',
+    database: 'Floristeria_la_Margarita'
+});
+
+if (db) {
+    console.log('Conexión a la base de datos establecida');
+} else {
+    console.error('');
+}
+
+module.exports = db;
